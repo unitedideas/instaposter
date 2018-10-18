b9704a663f8db5e0bac099eabed5a42b261a998d
Unitedideas <unitedideas@gmail.com>
2018-10-17 21:56:18 -0700

    still working on the post

--- poster.py
+++ poster.py
@@ -4,24 +4,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait  # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC  # available since 2.26.0
-
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 import secrets
 import time
-import os
-from tkinter import *
-# from tkinter.tkk import *
+import os, sys, subprocess
+import webbrowser
 
-
-def showImage():
-    os.system(r'/Users/shanecheek/Desktop/projects/instaposter/autopost/1/fd3844becd196550329031b0c7afdb57.jpg')
-
-
-root = Tk()
-root.title("InstaPicture")
-
-# from instapy import InstaPy
-# from instapy.util import smart_run
 options = Options()
 options.add_argument(
     '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
@@ -33,7 +22,7 @@ session.get('https://www.instagram.com/accounts/login/?source=mobile_nav_menu')
 insta_username = secrets.insta_username
 insta_password = secrets.insta_password
 
-# if on insa home page
+# if on insta home page
 try:
     usr_login_button = session.find_element_by_xpath(
         '//*[@id="react-root"]/section/main/article/div/div/div/div[2]/button')
@@ -81,13 +70,25 @@ WebDriverWait(session, 5).until(
         (By.XPATH, '//*[@id="react-root"]/section/nav[2]/div/div/div[2]/div/div/div[3]/span')))
 post_btn = session.find_element_by_xpath('//*[@id="react-root"]/section/nav[2]/div/div/div[2]/div/div/div[3]/span')
 post_btn.click()
-time.sleep(2)
 # post_btn.send_keys(r'/Users/shanecheek/Desktop/projects/instaposter/autopost/1/fd3844becd196550329031b0c7afdb57.jpg')
 # actions.send_keys('/Users/shanecheek/Desktop/projects/instagram_bot/autopost/fd3844becd196550329031b0c7afdb57.jpg')
 print('found it, clicked post!!!')
 time.sleep(2)
 
-showImage()
+payload = session.file_detector.is_local_file(
+    r"/Users/shanecheek/Desktop/projects/instaposter/autopost/1/fd3844becd196550329031b0c7afdb57.jpg")
+print(payload)
 
-# session.close()
-# session.quit()
+alert = session.switch_to.alert()
+alert.accept()
+def openFolder(path):
+    subprocess.Popen(['open', '--', path])
+
+
+openFolder('/Users/shanecheek/Desktop/projects/instaposter/autopost/1/fd3844becd196550329031b0c7afdb57.jpg')
+
+WebDriverWait(session, 5).until(
+    EC.element_to_be_clickable((By.XPATH, '//input[@type="file"]'))).send_keys(
+    "/Users/shanecheek/Desktop/projects/instaposter/autopost/1/fd3844becd196550329031b0c7afdb57.jpg")
+session.close()
+session.quit()
